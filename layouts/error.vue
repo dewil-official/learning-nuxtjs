cc79e8424b9d554851b92b678152fbd8042df4a7
Dennis W <contact@denniswillmann.com>
2020-03-31 12:00:09 +0200

    Template Cleanup - Removed all bloat that came by default

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,14 +1,8 @@
 <template>
   <v-app dark>
-    <h1 v-if="error.statusCode === 404">
-      {{ pageNotFound }}
-    </h1>
-    <h1 v-else>
-      {{ otherError }}
-    </h1>
-    <NuxtLink to="/">
-      Home page
-    </NuxtLink>
+    <h1 v-if="error.statusCode === 404">{{ pageNotFound }}</h1>
+    <h1 v-else>{{ otherError }}</h1>
+    <NuxtLink to="/">Back to landing page</NuxtLink>
   </v-app>
 </template>
 
@@ -21,13 +15,13 @@ export default {
       default: null
     }
   },
-  data () {
+  data() {
     return {
       pageNotFound: '404 Not Found',
       otherError: 'An error occurred'
     }
   },
-  head () {
+  head() {
     const title =
       this.error.statusCode === 404 ? this.pageNotFound : this.otherError
     return {
